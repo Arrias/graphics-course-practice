489c6c91911ca8309d4f3c7daf53bd649e3dc91d
Albert Skalt <allber971@gmail.com>
2022-09-21 21:50:03 +0300

    Add practice3 tasks 6-7

--- 2022/practice3/main.cpp
+++ 2022/practice3/main.cpp
@@ -255,10 +255,13 @@ int main() try {
 
         glUseProgram(program);
         glUniformMatrix4fv(view_location, 1, GL_TRUE, view);
-        glLineWidth(5.f);
-        glPointSize(10);
-        glDrawArrays(GL_LINE_STRIP, 0, (GLsizei) v.size());
-        glDrawArrays(GL_POINTS, 0, (GLsizei) v.size());
+
+        glBindVertexArray(curve_holder.getVao());
+        glDrawArrays(GL_LINE_STRIP, 0, (GLsizei) curve_holder.size());
+        glDrawArrays(GL_POINTS, 0, (GLsizei) curve_holder.size());
+
+        glBindVertexArray(bezier_holder.getVao());
+        glDrawArrays(GL_LINE_STRIP, 0, (GLsizei) bezier_holder.size());
 
         glBindVertexArray(curve_holder.getVao());
         glDrawArrays(GL_LINE_STRIP, 0, (GLsizei) curve_holder.size());
