1ae511d5bcb7c92eaa92e4f1d936c95db2d89501
Albert Skalt <allber971@gmail.com>
2022-09-26 19:43:18 +0300

    Add practice 4

--- 2022/practice4/main.cpp
+++ 2022/practice4/main.cpp
@@ -2,7 +2,9 @@
 #include <SDL.h>
 #undef main
 #else
+
 #include <SDL2/SDL.h>
+
 #endif
 
 #include <GL/glew.h>
@@ -16,23 +18,20 @@
 
 #include "obj_parser.hpp"
 
-std::string to_string(std::string_view str)
-{
+std::string to_string(std::string_view str) {
     return std::string(str.begin(), str.end());
 }
 
-void sdl2_fail(std::string_view message)
-{
+void sdl2_fail(std::string_view message) {
     throw std::runtime_error(to_string(message) + SDL_GetError());
 }
 
-void glew_fail(std::string_view message, GLenum error)
-{
+void glew_fail(std::string_view message, GLenum error) {
     throw std::runtime_error(to_string(message) + reinterpret_cast<const char *>(glewGetErrorString(error)));
 }
 
 const char vertex_shader_source[] =
-R"(#version 330 core
+        R"(#version 330 core
 
 uniform mat4 view;
 uniform mat4 transform;
@@ -50,7 +49,7 @@ void main()
 )";
 
 const char fragment_shader_source[] =
-R"(#version 330 core
+        R"(#version 330 core
 
 in vec3 normal;
 
@@ -63,15 +62,13 @@ void main()
 }
 )";
 
-GLuint create_shader(GLenum type, const char * source)
-{
+GLuint create_shader(GLenum type, const char *source) {
     GLuint result = glCreateShader(type);
     glShaderSource(result, 1, &source, nullptr);
     glCompileShader(result);
     GLint status;
     glGetShaderiv(result, GL_COMPILE_STATUS, &status);
-    if (status != GL_TRUE)
-    {
+    if (status != GL_TRUE) {
         GLint info_log_length;
         glGetShaderiv(result, GL_INFO_LOG_LENGTH, &info_log_length);
         std::string info_log(info_log_length, '\0');
@@ -81,8 +78,7 @@ GLuint create_shader(GLenum type, const char * source)
     return result;
 }
 
-GLuint create_program(GLuint vertex_shader, GLuint fragment_shader)
-{
+GLuint create_program(GLuint vertex_shader, GLuint fragment_shader) {
     GLuint result = glCreateProgram();
     glAttachShader(result, vertex_shader);
     glAttachShader(result, fragment_shader);
@@ -90,8 +86,7 @@ GLuint create_program(GLuint vertex_shader, GLuint fragment_shader)
 
     GLint status;
     glGetProgramiv(result, GL_LINK_STATUS, &status);
-    if (status != GL_TRUE)
-    {
+    if (status != GL_TRUE) {
         GLint info_log_length;
         glGetProgramiv(result, GL_INFO_LOG_LENGTH, &info_log_length);
         std::string info_log(info_log_length, '\0');
@@ -102,8 +97,7 @@ GLuint create_program(GLuint vertex_shader, GLuint fragment_shader)
     return result;
 }
 
-int main() try
-{
+int main() try {
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
         sdl2_fail("SDL_Init: ");
 
@@ -118,11 +112,11 @@ int main() try
     SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
-    SDL_Window * window = SDL_CreateWindow("Graphics course practice 4",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        800, 600,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED);
+    SDL_Window *window = SDL_CreateWindow("Graphics course practice 4",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          800, 600,
+                                          SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED);
 
     if (!window)
         sdl2_fail("SDL_CreateWindow: ");
@@ -152,36 +146,98 @@ int main() try
     std::string project_root = PROJECT_ROOT;
     obj_data bunny = parse_obj(project_root + "/bunny.obj");
 
+    glEnable(GL_DEPTH_TEST);
+
+    GLuint vao, vbo, ebo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t) * bunny.indices.size(), bunny.indices.data(), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(obj_data::vertex) * bunny.vertices.size(), bunny.vertices.data(), GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(obj_data::vertex), (void *) nullptr);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(obj_data::vertex), (void *) (sizeof(float) * 3));
+
+    glEnable(GL_CULL_FACE);
+    //glCullFace(GL_FRONT);
+
     auto last_frame_start = std::chrono::high_resolution_clock::now();
 
     float time = 0.f;
 
+    auto id = [&](int i, int j) {
+        return i * 4 + j;
+    };
+
+    auto getTransform = [&](int c1, int c2, float scale, float angle, float x, float y, float z) {
+        float A = cos(angle) * scale;
+        float B = -sin(angle) * scale;
+        float C = scale;
+        float D = sin(angle) * scale;
+        float E = cos(angle) * scale;
+
+        std::vector<float> transform = {
+                0.0, 0.f, 0.0, x,
+                0.f, 0.f, 0.f, y,
+                0.f, 0.f, 0.f, z,
+                0.f, 0.f, 0.f, 1.f,
+        };
+        transform[id(c1, c1)] = A;
+        transform[id(c1, c2)] = B;
+        transform[id(c2, c1)] = D;
+        transform[id(c2, c2)] = E;
+
+        int other = 3 - c1 - c2;
+        transform[id(other, other)] = C;
+
+        return transform;
+    };
+
     std::map<SDL_Keycode, bool> button_down;
 
+    float ratio = float(width) / float(height);
+    float theta = acos(-1) / 2;
+
+    float near = 0.01;
+    float far = 150.0;
+    float right = near * tan(theta / 2);
+    float top = right / ratio;
+    float speed = 1.0;
+
+    float bunny_x = 0;
+    float bunny_y = 0;
+    float bunny_z = -3;
+
     bool running = true;
-    while (running)
-    {
-        for (SDL_Event event; SDL_PollEvent(&event);) switch (event.type)
-        {
-        case SDL_QUIT:
-            running = false;
-            break;
-        case SDL_WINDOWEVENT: switch (event.window.event)
-            {
-            case SDL_WINDOWEVENT_RESIZED:
-                width = event.window.data1;
-                height = event.window.data2;
-                glViewport(0, 0, width, height);
-                break;
+    while (running) {
+        for (SDL_Event event; SDL_PollEvent(&event);)
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = false;
+                    break;
+                case SDL_WINDOWEVENT:
+                    switch (event.window.event) {
+                        case SDL_WINDOWEVENT_RESIZED:
+                            width = event.window.data1;
+                            height = event.window.data2;
+                            glViewport(0, 0, width, height);
+                            break;
+                    }
+                    break;
+                case SDL_KEYDOWN:
+                    button_down[event.key.keysym.sym] = true;
+                    break;
+                case SDL_KEYUP:
+                    button_down[event.key.keysym.sym] = false;
+                    break;
             }
-            break;
-        case SDL_KEYDOWN:
-            button_down[event.key.keysym.sym] = true;
-            break;
-        case SDL_KEYUP:
-            button_down[event.key.keysym.sym] = false;
-            break;
-        }
 
         if (!running)
             break;
@@ -191,27 +247,49 @@ int main() try
         last_frame_start = now;
         time += dt;
 
+        if (button_down[SDLK_LEFT]) {
+            bunny_x -= speed * dt;
+        }
+        if (button_down[SDLK_RIGHT]) {
+            bunny_x += speed * dt;
+        }
+
+        if (button_down[SDLK_UP]) {
+            bunny_y += speed * dt;
+        }
+        if (button_down[SDLK_DOWN]) {
+            bunny_y -= speed * dt;
+        }
+
         glClear(GL_COLOR_BUFFER_BIT);
 
+        float angle = time;
+        float scale = 0.5;
+
         float view[16] =
-        {
-            1.f, 0.f, 0.f, 0.f,
-            0.f, 1.f, 0.f, 0.f,
-            0.f, 0.f, 1.f, 0.f,
-            0.f, 0.f, 0.f, 1.f,
-        };
+                {
+                        near / right, 0.f, 0.f, 0.f,
+                        0.f, near / top, 0.f, 0.f,
+                        0.f, 0.f, -(far + near) / (far - near), -(2 * far * near) / (far - near),
+                        0.f, 0.f, -1, 0.f,
+                };
 
-        float transform[16] =
-        {
-            1.f, 0.f, 0.f, 0.f,
-            0.f, 1.f, 0.f, 0.f,
-            0.f, 0.f, 1.f, 0.f,
-            0.f, 0.f, 0.f, 1.f,
-        };
-
+        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
         glUseProgram(program);
         glUniformMatrix4fv(view_location, 1, GL_TRUE, view);
-        glUniformMatrix4fv(transform_location, 1, GL_TRUE, transform);
+
+        float dx[] = {-1.75, 0, 1.75};
+        float dy[] = {1.75, 0, -1.75};
+
+        int num = 0;
+        for (int c1 = 0; c1 < 3; ++c1) {
+            for (int c2 = c1 + 1; c2 < 3; ++c2) {
+                auto t_curr = getTransform(c1, c2, scale, angle, bunny_x + dx[num], bunny_y + dy[num], bunny_z);
+                glUniformMatrix4fv(transform_location, 1, GL_TRUE, t_curr.data());
+                glDrawElements(GL_TRIANGLES, bunny.indices.size(), GL_UNSIGNED_INT, (void *) nullptr);
+                num++;
+            }
+        }
 
         SDL_GL_SwapWindow(window);
     }
@@ -219,8 +297,7 @@ int main() try
     SDL_GL_DeleteContext(gl_context);
     SDL_DestroyWindow(window);
 }
-catch (std::exception const & e)
-{
+catch (std::exception const &e) {
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
 }
