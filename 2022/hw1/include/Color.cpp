8a17ff46be73bc3554612df5a6e998df854ffb7e
Arrias <allber971@gmail.com>
2022-10-03 02:30:59 +0300

    Add isolines

--- 2022/hw1/include/Color.cpp
+++ 2022/hw1/include/Color.cpp
@@ -6,3 +6,11 @@ Color::Color(uint8_t r, uint8_t g, uint8_t b) {
     data[2] = b;
     data[3] = 0;
 }
+
+Color BlueColor() {
+    return Color(0, 0, 255);
+}
+
+Color YellowColor() {
+    return Color(255, 255, 0);
+}
