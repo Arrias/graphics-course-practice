7f2f69f52fa50aefe96c9ebd5a353316871c085d
Arrias <allber971@gmail.com>
2022-10-02 15:40:23 +0300

    Refactor code, add smooth transfusion

--- 2022/hw1/main.cpp
+++ 2022/hw1/main.cpp
@@ -17,40 +17,16 @@
 #include <climits>
 #include <chrono>
 #include <random>
+#include <cassert>
 
-const int baseWidth = 800;
-const int baseHeight = 600;
+const int GridHeight = 300;
+const int GridWidth = 300;
+const int WindowWidth = 800;
+const int WindowHeight = 600;
+
+const float eps = 0.05;
 const std::string program_name = "Crazy functions";
 std::mt19937 rnd(std::chrono::steady_clock::now().time_since_epoch().count());
-
-template<class T>
-T sqr(T x) {
-    return x * x;
-}
-
-struct Vec2 {
-    float x{}, y{};
-
-    float len() {
-        return x * x + y * y;
-    }
-};
-
-Vec2 operator+(Vec2 a, Vec2 b) {
-    return Vec2{a.x + b.x, a.y + b.y};
-}
-
-Vec2 operator-(Vec2 a, Vec2 b) {
-    return Vec2{a.x - b.x, a.y - b.y};
-}
-
-Vec2 operator*(float sc, Vec2 a) {
-    return {a.x * sc, a.y * sc};
-}
-
-struct Color {
-    std::uint8_t data[4]{};
-};
 
 const std::string vertex_shader_source =
         R"(#version 330 core
@@ -77,6 +53,44 @@ const std::string fragment_shader_source =
             }
 )";
 
+template<class T>
+T sqr(T x);
+
+bool equal(float a, float b);
+
+bool lesser(float a, float b);
+
+struct Vec2 {
+    float x{}, y{};
+
+    Vec2(float x, float y) : x(x), y(y) {}
+
+    float square_len() const {
+        return x * x + y * y;
+    }
+
+    void move90() {
+        std::swap(x, y);
+        x *= -1.f;
+    }
+};
+
+Vec2 operator+(Vec2 a, Vec2 b) {
+    return Vec2{a.x + b.x, a.y + b.y};
+}
+
+Vec2 operator-(Vec2 a, Vec2 b) {
+    return Vec2{a.x - b.x, a.y - b.y};
+}
+
+Vec2 operator*(float sc, Vec2 a) {
+    return {a.x * sc, a.y * sc};
+}
+
+struct Color {
+    std::uint8_t data[4]{};
+};
+
 std::string to_string(std::string_view str);
 
 void sdl2_fail(std::string_view message);
@@ -89,29 +103,13 @@ GLuint create_program(GLuint vertex_shader, GLuint fragment_shader);
 
 void SDL_init(SDL_Window *&window, SDL_GLContext &gl_context);
 
-void gen_view_matrix(float *m, float width, float height) {
-    float view[16] = {
-            2.f / (float) width, 0.f, 0.f, -1.f,
-            0.f, -2.f / (float) height, 0.f, 1.f,
-            0.f, 0.f, 1.f, 0.f,
-            0.f, 0.f, 0.f, 1.f,
-    };
-    memcpy(m, view, sizeof(float) * 16);
-}
+void gen_view_matrix(float *m, float width, float height);
 
 template<class T>
-void bindData(GLuint array_type, GLuint vbo, const std::vector<T> &vec) {
-    glBindBuffer(array_type, vbo);
-    glBufferData(array_type, sizeof(T) * vec.size(), vec.data(), GL_STATIC_DRAW);
-}
+void bindData(GLuint array_type, GLuint vbo, const std::vector<T> &vec);
 
 template<class T>
-void bindArgument(GLuint array_type, GLuint vbo, GLuint vao, size_t arg, GLint size, GLenum type, GLboolean norm, const GLvoid *pointer) {
-    glBindVertexArray(vao);
-    glBindBuffer(array_type, vbo);
-    glEnableVertexAttribArray(arg);
-    glVertexAttribPointer(arg, size, type, norm, sizeof(T), pointer);
-}
+void bindArgument(GLuint array_type, GLuint vbo, GLuint vao, size_t arg, GLint size, GLenum type, GLboolean norm, const GLvoid *pointer);
 
 struct Timer {
     std::chrono::time_point<std::chrono::high_resolution_clock> last_frame_start = std::chrono::high_resolution_clock::now();
@@ -172,7 +170,7 @@ struct Grid : public PointsHolder {
 
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
-                points.push_back({(float) i * shift_x, (float) j * shift_y});
+                points.emplace_back((float) i * shift_x, (float) j * shift_y);
                 if (i + 1 < n && j + 1 < m) {
                     // triangulation
                     ids.push_back(get_id(i, j));
@@ -199,56 +197,44 @@ struct Grid : public PointsHolder {
     }
 };
 
-const uint32_t COLOR_BASE = 256;
-const uint32_t COLORS_CNT = COLOR_BASE * COLOR_BASE * COLOR_BASE;
-
-Color get_color_by_number(uint32_t num) {
-    Color ret;
-    for (int i = 0; i < 3; ++i) {
-        ret.data[i] = num % COLOR_BASE;
-        num /= COLOR_BASE;
-    }
-    return ret;
+int getRnd(int l, int r) {
+    return (int) (rnd() % (r - l + 1) + l);
 }
 
-int getRnd(int l, int r) {
-    return rnd() % (r - l + 1) + l;
+float floatRnd(int l, int r) {
+    return (float) getRnd(l, r);
 }
 
 const auto sq2 = (float) sqrt(2);
 const std::vector<Vec2> directions = {
-        //{0.f,  1.f},
+        {0.f,  1.f},
         {sq2,  sq2},
-        // {1.f,  0.f},
+        {1.f,  0.f},
         {sq2,  -sq2},
-        //  {0.f,  -1.f},
+        {0.f,  -1.f},
         {-sq2, -sq2},
-        //  {-1.f, 0.f},
+        {-1.f, 0.f},
         {-sq2, sq2},
 };
 
 struct Circle {
-    Vec2 c;
-    float r;
-    Vec2 dir;
-    int potencial;
+    Vec2 center, direction;
+    float radius;
+    float r, g, b;
 
-    bool is_lie(Vec2 pos) const {
-        return sqr(pos.x - c.x) + sqr(pos.y - c.y) <= r * r;
+    Circle(Vec2 center, Vec2 direction, float radius, float r, float g, float b) :
+            center(center), direction(direction), radius(radius), r(r), g(g), b(b) {}
+
+    float f(Vec2 pos) const {
+        return sqr(radius) / (pos - center).square_len();
     }
 
     void move(float time, float width, float height) {
-        c = c + time * dir;
-        potencial--;
-        if (c.x <= 0 || c.y <= 0 || c.x >= width || c.y >= height) {
-            c = c - 2 * time * dir;
-            float xx = dir.x;
-            float yy = dir.y;
-            dir = {-yy, xx};
-            potencial = getRnd(10, 100);
-        } else if (potencial == 0) {
-            dir = directions[getRnd(0, (int) directions.size() - 1)];
-            potencial = getRnd(10, 100);
+        center = center + time * direction;
+        if (center.x <= 0 || center.y <= 0 || center.x >= width || center.y >= height) {
+            center = center - 2 * time * direction;
+            direction.move90();
+            direction.move90();
         }
     }
 };
@@ -268,34 +254,67 @@ int main(int argc, char **argv) try {
     // get uniforms
     GLuint view_location = glGetUniformLocation(program, "view");
 
-    const int GRID_WIDTH = 300;
-    const int GRID_HEIGHT = 300;
-
     // setup some parameters of painting
     glClearColor(0.8f, 0.8f, 1.f, 0.f);
     glPointSize(5);
     int width, height;
     SDL_GetWindowSize(window, &width, &height);
 
-    auto get_rand_cord = [&GRID_HEIGHT, &GRID_WIDTH]() {
-        uint32_t x = rnd() % (GRID_WIDTH - 20) + 10;
-        uint32_t y = rnd() % (GRID_HEIGHT - 20) + 10;
-        return Vec2{float(x), float(y)};
+    auto get_rand_cord = [ ](int delta) {
+        uint32_t x = getRnd(delta, GridWidth - delta);
+        uint32_t y = getRnd(delta, GridHeight - delta);
+        return Vec2(float(x), float(y));
     };
 
     std::vector<Circle> circles;
-    for (int i = 0; i < 6; ++i) {
-        circles.push_back(
-                Circle{get_rand_cord(), float(rnd() % 50 + 20),
-                       directions[rnd() % directions.size()]});
+//    circles.push_back(Circle({GridWidth / 2, GridHeight / 2}, directions[0], 20.f, 5, 0, 0));
+//    circles.push_back(Circle({GridWidth / 2, GridHeight / 2}, directions[4], 20.f, 0, 5, 0));
+
+    for (int i = 0; i < 10; ++i) {
+        auto center = get_rand_cord(20);
+        auto new_circle = Circle(center, directions[rnd() % directions.size()], 20.f, floatRnd(0, 1) * 5.f, floatRnd(0, 1) * 5.f,
+                                 floatRnd(0, 1) * 5.f);
+        circles.push_back(new_circle);
+
+//        if (getRnd(0, 1)) {
+//            auto new_circle = Circle(center, directions[rnd() % directions.size()], 20.f, 1, 0, 0);
+//            circles.push_back(new_circle);
+//        } else {
+//            auto new_circle = Circle(center, directions[rnd() % directions.size()], 20.f, 1, 1, 0);
+//            circles.push_back(new_circle);
+//        }
     }
 
     int grid_n = 100;
     int grid_m = 100;
     Grid grid;
-    grid.build(grid_n, grid_m, 300, 300);
+    grid.build(grid_n, grid_m, GridWidth, GridHeight);
 
-    const float MAX_VALUE = 5.f;
+    const std::vector<std::pair<float, unsigned>> limits = {
+            {0.0,  0u},
+            {1.f,  120u},
+            {1.5,  170u},
+            {2.f,  190u},
+            {2.5,  220u},
+            {3.f,  240u},
+            {1e9f, 255u},
+    };
+
+    auto get_component = [&](float dst) {
+        if (lesser(dst, 0.5f)) return 1u;
+        dst = std::min(dst, limits.back().first - 3 * eps);
+
+        for (int i = 1; i < limits.size(); ++i) {
+            auto [cur_lim, cur_col_lim] = limits[i];
+            if (lesser(dst, cur_lim)) {
+                auto [prv_lim, prv_col_lim] = limits[i - 1];
+                float frac = (dst - prv_lim) / (cur_lim - prv_lim);
+                return (unsigned) ((float) prv_col_lim + (float(cur_col_lim - prv_col_lim)) * frac);
+            }
+        }
+
+        assert(false);
+    };
 
     Timer timer;
     bool running = true;
@@ -309,11 +328,11 @@ int main(int argc, char **argv) try {
                     if (event.key.keysym.sym == SDLK_DOWN) {
                         grid_n -= 5;
                         grid_m -= 5;
-                        grid.build(grid_n, grid_m, 300, 300);
+                        grid.build(grid_n, grid_m, GridWidth, GridHeight);
                     } else if (event.key.keysym.sym == SDLK_UP) {
                         grid_n += 5;
                         grid_m += 5;
-                        grid.build(grid_n, grid_m, 300, 300);
+                        grid.build(grid_n, grid_m, GridWidth, GridHeight);
                     }
             }
             if (event.type == SDL_QUIT) {
@@ -327,26 +346,35 @@ int main(int argc, char **argv) try {
 
         float view[16]{};
         gen_view_matrix(view, (float) width, (float) height);
-        auto dt = timer.tick() * 50;
+        auto dt = timer.tick();
 
         for (auto &circle: circles) {
-            circle.move(dt, GRID_WIDTH, GRID_HEIGHT);
+            circle.move(dt * 50.f, GridWidth, GridHeight);
         }
 
-        grid.apply(std::function<Color(Vec2)>([&timer, &circles, &MAX_VALUE](Vec2 pos) {
+        grid.apply(std::function<Color(Vec2)>([&timer, &circles, &get_component](Vec2 pos) {
+            float r_dst = 0, g_dst = 0, b_dst = 0;
             float dst = 0;
             for (Circle &circle: circles) {
-                dst += ((circle.r * circle.r) / (circle.c - pos).len());
+                float add = circle.f(pos);
+                r_dst += circle.r * add;
+                g_dst += circle.g * add;
+                b_dst += circle.b * add;
+                dst += add;
             }
-            dst = std::min(dst, MAX_VALUE);
-            float len = MAX_VALUE / 255;
             Color ret;
-            ret.data[0] = int(floor(dst / len));
+            if (lesser(dst, 1.0)) return ret;
+            ret.data[0] = get_component(r_dst);
+            ret.data[1] = get_component(g_dst);
+            ret.data[2] = get_component(b_dst);
             return ret;
         }));
 
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // set uniform
         glUniformMatrix4fv((GLint) view_location, 1, GL_TRUE, view);
+
         glBindVertexArray(grid.vao);
         glDrawElements(GL_TRIANGLES, (GLsizei) grid.size(), GL_UNSIGNED_INT, (void *) nullptr);
         SDL_GL_SwapWindow(window);
@@ -367,7 +395,7 @@ void SDL_init(SDL_Window *&window, SDL_GLContext &gl_context) {
     window = SDL_CreateWindow(program_name.data(),
                               SDL_WINDOWPOS_CENTERED,
                               SDL_WINDOWPOS_CENTERED,
-                              baseWidth, baseHeight,
+                              WindowWidth, WindowHeight,
                               SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED);
 
     if (!window)
@@ -433,4 +461,41 @@ GLuint create_program(GLuint vertex_shader, GLuint fragment_shader) {
     }
 
     return result;
+}
+
+void gen_view_matrix(float *m, float width, float height) {
+    float view[16] = {
+            2.f / (float) width, 0.f, 0.f, -1.f,
+            0.f, -2.f / (float) height, 0.f, 1.f,
+            0.f, 0.f, 1.f, 0.f,
+            0.f, 0.f, 0.f, 1.f,
+    };
+    memcpy(m, view, sizeof(float) * 16);
+}
+
+template<class T>
+void bindData(GLuint array_type, GLuint vbo, const std::vector<T> &vec) {
+    glBindBuffer(array_type, vbo);
+    glBufferData(array_type, sizeof(T) * vec.size(), vec.data(), GL_STATIC_DRAW);
+}
+
+template<class T>
+void bindArgument(GLuint array_type, GLuint vbo, GLuint vao, size_t arg, GLint size, GLenum type, GLboolean norm, const GLvoid *pointer) {
+    glBindVertexArray(vao);
+    glBindBuffer(array_type, vbo);
+    glEnableVertexAttribArray(arg);
+    glVertexAttribPointer(arg, size, type, norm, sizeof(T), pointer);
+}
+
+template<class T>
+T sqr(T x) {
+    return x * x;
+}
+
+bool equal(float a, float b) {
+    return fabs(a - b) < eps;
+}
+
+bool lesser(float a, float b) {
+    return a + eps < b;
 }
